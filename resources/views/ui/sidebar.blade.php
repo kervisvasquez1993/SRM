fb86dc5b75a00cd9d37a991b8fedaf5299d2f256
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-04 14:48:53 -0400

    aun sin finalizar la base de datos

---
+++ resources/views/ui/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="../assets/img/sidebar-1.jpg">
+    <div class="logo"><a href="" class="simple-text logo-normal">
+        SRM Dynamics
+      </a></div>
+     <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="nav-item active  ">
+          <a class="nav-link" href="./dashboard.html">
+            <i class="material-icons">dashboard</i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="./user.html">
+            <i class="material-icons">person</i>
+            <p>Perfil</p>
+          </a>
+        </li>
+      </ul>
+    </div> 
+  </div>
