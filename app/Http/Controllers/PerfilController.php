3c1d67eb27cffc6d130ef18ab8c7232677d305c7
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-04 15:19:26 -0400

    agregamos cambios desde srm-13

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -14,7 +14,7 @@ class PerfilController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -24,7 +24,7 @@ class PerfilController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
