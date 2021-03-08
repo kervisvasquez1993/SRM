f5d72328c5570e5d773a7be06f11170f4777b0f1
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-08 19:41:59 -0400

    add vue sweealert

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -58,7 +58,7 @@ class PerfilController extends Controller
      */
     public function edit(Perfil $perfil)
     {
-        //
+        return view('perfil.edit', compact('perfil'));
     }
 
     /**
---
+++ resources/views/perfil/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.dashboar')
+@section('content')
+
+
+@endsection
