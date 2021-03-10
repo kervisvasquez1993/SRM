ac2a53f9d09bd2a692460516ae31a57bf2d856dd
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-10 09:11:06 -0400

    :bug: bugfix-43 change in material-dashboar.css.map for dashboard-free.map and deleted link of google map in admin.dashboar

--- resources/views/admin/dashboar.blade.php
+++ resources/views/admin/dashboar.blade.php
@@ -78,7 +78,7 @@
   <!-- Library for adding dinamically elements -->
   <script src="{{asset('assets/js/plugins/arrive.min.js')}}"></script>
   <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE')}}"></script>
+  
   <!-- Chartist JS -->
   <script src="{{asset('assets/js/plugins/chartist.min.js')}}"></script>
   <!--  Notifications Plugin    -->
