af6bc7d0f929f80de20151eadde28ec47cdefcd2
carlosempd <carlosempd@gmail.com>
2021-03-10 08:14:37 -0400

    :sparkles: SRM-43 Create vue component

---
+++ resources/js/components/EditArtStatus.vue
@@ -0,0 +1,15 @@
+<template>
+    <div @click="editStatus">
+        <h1>DESDE VUE</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        editStatus() {
+            console.log('EDITAR');
+        }
+    },
+}
+</script>
