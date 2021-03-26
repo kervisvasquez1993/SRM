c165875cd4ba6a9f073c1b6d86bc4242d913d76e
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-26 09:15:38 -0400

    :twisted_rightwards_arrows: pull from develop

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,11 +10,7 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 
 // Axios
 window.axios = require('axios');
-
-
 window.Vue = require('vue');
-
-
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
