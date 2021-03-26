ce5ce2b90638474398c5848be0c0c80f920adc96
carlosempd <carlosempd@gmail.com>
2021-03-26 14:38:59 -0400

    :lipstick: Format html code for incidencias

--- resources/views/ui/borrar-incidencia.blade.php
+++ resources/views/ui/borrar-incidencia.blade.php
@@ -1,4 +1,11 @@
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div 
+  class="modal fade" 
+  id="deleteModal" 
+  tabindex="-1" 
+  role="dialog" 
+  aria-labelledby="exampleModalLabel" 
+  aria-hidden="true"
+>
 
     <div class="modal-dialog" role="document">
       <div class="modal-content">
@@ -7,12 +14,26 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
+
         <div class="modal-body">
           ¿Está seguro que desea eliminar ésta incidencia?
         </div>
+
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary btn-round" data-dismiss="modal">Cerrar</button>
-          <button onclick="elimiarIncidencia('{{ $path }}')" type="button" class="btn btn-outline-primary btn-round">Eliminar</button>
+          <button 
+            type="button" 
+            class="btn btn-secondary btn-round" 
+            data-dismiss="modal"
+          >
+            Cerrar
+          </button>
+
+          <button 
+            onclick="elimiarIncidencia('{{ $path }}')" 
+            type="button" 
+            class="btn btn-outline-primary btn-round">
+            Eliminar
+          </button>
         </div>
       </div>
     </div>
