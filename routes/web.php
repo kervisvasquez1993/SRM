585edd7cde878e9484e8711ee64c1d7c547db715
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-31 11:47:51 -0400

    🛂 appy midlleware od authentication in routes.web

--- routes/web.php
+++ routes/web.php
@@ -18,51 +18,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/admin', function () {
-    return view('admin.dashboar');
-});
-
 Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-//perfil view
-Route::resource('/perfil', 'PerfilController');
-//tareas 
-Route::resource('/tareas', 'TareaController');
-Route::put('/negociaciones/{negociar}', 'ProveedorController@Negociar')->name('negociaciones.update');
-Route::resource('/proveedores', 'ProveedorController');
-// Bocetos
-Route::resource('/bocetos', 'BocetoController');
-
-// Validacion de Bocetos
-Route::resource('/validacion-bocetos', 'ValidacionBocetoController');
-
-// Fichas
-Route::resource('/fichas', 'FichaController');
-
-// Validacion de Fichas
-Route::resource('/validacion-fichas', 'ValidacionFichaController');
-
-// Confirmacion Proveedor
-Route::resource('/confirmacion-proveedor', 'ConfirmacionProveedorController');
-
-// Arte
-Route::get('/artes/search', 'ArteController@search')->name('artes.search');
-Route::resource('/artes', 'ArteController');
-Route::get('/usuarios', 'TareaController@usuarios');
-
-
-/* lista de proveedor aprobado */
-Route::get('/proveedor-negociacion', 'ProveedorController@listaAprobado')->name('proveedor-negociacion');  
-/* aprobar artes  actualizar*/
-
-Route::put('/arteAprobados/{arteAprobado}', 'PivotTareaProveederController@arteAprobado')->name('arteAprobados.update');
-
-/* fin arte aprobado */
-
-
-Route::put('/produccionAprobados/{produccionAprobado}', 'PivotTareaProveederController@produccionAprobado')->name('produccionAprobados.update');
-Route::put('/arteProduccionAprobados/{arteProduccionAprobado}', 'PivotTareaProveederController@arteProduccionAprobado')->name('arteProduccionAprobados.update');
-
-// Productos
-Route::resource('productos', 'ProductoController');
+Route::group(['middleware' => 'auth'], function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+        //perfil view
+        Route::resource('/perfil', 'PerfilController');
+        //tareas 
+        Route::resource('/tareas', 'TareaController');
+        Route::put('/negociaciones/{negociar}', 'ProveedorController@Negociar')->name('negociaciones.update');
+        Route::resource('/proveedores', 'ProveedorController');
+        // Bocetos
+        Route::resource('/bocetos', 'BocetoController');
+        
+        // Validacion de Bocetos
+        Route::resource('/validacion-bocetos', 'ValidacionBocetoController');
+        
+        // Fichas
+        Route::resource('/fichas', 'FichaController');
+        
+        // Validacion de Fichas
+        Route::resource('/validacion-fichas', 'ValidacionFichaController');
+        
+        // Confirmacion Proveedor
+        Route::resource('/confirmacion-proveedor', 'ConfirmacionProveedorController');
+        
+        // Arte
+        Route::get('/artes/search', 'ArteController@search')->name('artes.search');
+        Route::resource('/artes', 'ArteController');
+        Route::get('/usuarios', 'TareaController@usuarios');
+        
+        
+        /* lista de proveedor aprobado */
+        Route::get('/proveedor-negociacion', 'ProveedorController@listaAprobado')->name('proveedor-negociacion');  
+        /* aprobar artes  actualizar*/
+        
+        Route::put('/arteAprobados/{arteAprobado}', 'PivotTareaProveederController@arteAprobado')->name('arteAprobados.update');
+        
+        /* fin arte aprobado */
+        
+        
+        Route::put('/produccionAprobados/{produccionAprobado}', 'PivotTareaProveederController@produccionAprobado')->name('produccionAprobados.update');
+        Route::put('/arteProduccionAprobados/{arteProduccionAprobado}', 'PivotTareaProveederController@arteProduccionAprobado')->name('arteProduccionAprobados.update');
+         // Productos
+        Route::resource('productos', 'ProductoController');
+});
