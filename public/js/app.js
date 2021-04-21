c7076e9d991a9d2e827569006d7b40efed6bb2fe
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-04-21 10:04:29 -0400

    from disk of job

--- public/js/app.js
+++ public/js/app.js
@@ -19289,8 +19289,8 @@ document.addEventListener('DOMContentLoaded', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/SRMDynamics/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/SRMDynamics/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/srm/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/srm/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
