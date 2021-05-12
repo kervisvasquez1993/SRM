6e2fc06ab07a595086b81c936a8403f31b45e3b9
kervis vasquez <kervisvasquez24@gmail.com>
2021-05-12 18:28:17 -0400

    agregamos tabla para ordenar y filtrar usuario

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -19,12 +19,36 @@ class ShowPosts extends Component
   */
 
     public $message;
+    public $sort = 'id';
+    public $orden = 'asc';
     public function render()
     {
         /* $produccion = ProduccionTransito::all(); */
         $produccion = User::where('name', 'like', '%'.$this->message.'%')
                             ->orWhere('email', 'like', '%'.$this->message.'%')
+                            ->orderBy($this->sort, $this->orden)
                             ->get();
         return view('livewire.show-posts', compact(('produccion')))->layout('layouts.base');
     }
+
+    public function order($sort)
+     {
+            if($this->sort == $sort)
+            {
+                if($this->orden = 'asc')
+                {
+                    $this->orden = 'desc';
+                }
+                else
+                {
+                    $this->orden = 'asc';
+                }
+            }
+            else
+            {
+                $this->sort = $sort;
+                $this->orden = 'asc';
+
+            }
+     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -5,11 +5,26 @@
         <input wire:model="message" type="text">
       {{-- <h1>{{ $message }}</h1> --}}
       @if($produccion->count())
-           @foreach($produccion as $producto)
-               <div>    
-                  {{$producto}}
-               </div>
-           @endforeach
+        <table class="table">
+            <thead>
+               <tr>
+                 <th wire:click="order('id')" scope="col">id</th>
+                 <th wire:click="order('rol')" scope="col">Rol</th>
+                 <th wire:click="order('name')" scope="col">Nombre</th>
+                 <th wire:click="order('email')" scope="col">Email</th>
+               </tr>
+            </thead> 
+            @foreach($produccion as $producto)
+                <tbody>
+                  <tr>
+                    <th scope="row">{{$producto->id}}</th>
+                    <td>{{$producto->rol}}</td>
+                    <td>{{$producto->name}}</td>
+                    <td>{{$producto->email}}</td>
+                  </tr>
+                </tbody>
+            @endforeach
+          </table>
            @else
            <div> No existe reultado de busqueda asociado a <span class="font-weight-bold">{{$message}}</span></div>
         @endif
