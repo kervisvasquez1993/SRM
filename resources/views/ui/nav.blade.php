be9ce851d565dc34cb4b96ee940aa5900a60eaca
carlosempd <carlosempd@gmail.com>
2021-05-28 09:51:26 -0400

    :art: feature/SRM-88 Format sidebar.blade.php

--- resources/views/ui/nav.blade.php
+++ resources/views/ui/nav.blade.php
@@ -1,52 +1,54 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
     <div class="container-fluid">
-      <div class="navbar-wrapper">
-        <button id="botonSidebar">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-          </svg>
+        <div class="navbar-wrapper">
+            <button id="botonSidebar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
+            </button>
+            <a class="navbar-brand" href="javascript:;">Dashboard</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
+            <span class="navbar-toggler-icon icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="javascript:;">Dashboard</a>
-      </div>
+        <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="navbar-toggler-icon icon-bar"></span>
-        <span class="navbar-toggler-icon icon-bar"></span>
-        <span class="navbar-toggler-icon icon-bar"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end">
-      
-        <ul class="navbar-nav">
-         
-          <li class="nav-item">
-            <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="material-icons">notifications</i>
-              <span class="notification">5</span>
-              <p class="d-lg-none d-md-block">
-                Some Actions
-              </p>
-            </a>
-          </li>
-          <li class="nav-item ">
-           
-              <p class="d-lg-none d-md-block">
-                Account
-              </p>
-              {{Auth::user()->name}}
-          </li>
-          <li class="nav-item mx-2 p-2">
-            <a class="" href="{{ route('logout') }}"
-              onclick="event.preventDefault();
+                <li class="nav-item">
+                    <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons">notifications</i>
+                        <span class="notification">5</span>
+                        <p class="d-lg-none d-md-block">
+                            Some Actions
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item ">
+
+                    <p class="d-lg-none d-md-block">
+                        Account
+                    </p>
+                    {{ Auth::user()->name }}
+                </li>
+                <li class="nav-item mx-2 p-2">
+                    <a class="" href="{{ route('logout') }}" onclick="event.preventDefault();
                             document.getElementById('logout-form').submit();">
-               {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" >
-              @csrf
-            </form>
-          </li>
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                    </form>
+                </li>
 
-        </ul>
-      </div>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
