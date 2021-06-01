f3671cfd1e7d4d27257ffbaa659410576fd62097
kervis vasquez <kervisvasquez24@gmail.com>
2021-06-01 11:08:48 -0400

    🏗️  feature/SRM-123 GENERALIZING ANSWER FROM OUR API

---
+++ app/Traits/ApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+
+trait ApiResponse
+{
+    private function successResponse($data, $code)
+    {
+        return response()->json($data, $code);
+    }
+    protected function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        return $this->successResponse(['data' => $collection], $code);
+    }
+    protected function showOne(Model $instace, $code = 200)
+    {
+        return $this->successResponse(['data' => $instace], $code);
+    }
+}
