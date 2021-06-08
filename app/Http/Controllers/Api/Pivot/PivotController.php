3b7a6959e80966f1ad068a30e8968f8a5e58e247
kervis vasquez <kervisvasquez24@gmail.com>
2021-06-08 10:49:02 -0400

    ✨ big-fix initialize art and production

--- app/Http/Controllers/Api/Pivot/PivotController.php
+++ app/Http/Controllers/Api/Pivot/PivotController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Api\Pivot;
 
+use App\Arte;
+use Carbon\Carbon;
+use App\ProduccionTransito;
 use App\PivotTareaProveeder;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -36,7 +39,9 @@ class PivotController extends ApiController
         $pivot = PivotTareaProveeder::findOrFail($pivotTareaProveederId);
         $pivot->iniciar_arte = 1;
         $pivot->save();
+        $this->artesCreate($pivot->id, $pivot->compra->item); 
         $pivotResource = new PivotTareaProveederResource($pivot);
+        
         return $this->showOneResource($pivotResource);
         
     }
@@ -46,8 +51,41 @@ class PivotController extends ApiController
         $pivot = PivotTareaProveeder::findOrFail($pivotTareaProveederId);
         $pivot->iniciar_produccion = 1;
         $pivot->save();
+        $this->produccionTransitoCreate($pivot->id);
         $pivotResource = new PivotTareaProveederResource($pivot);
+        
         return $this->showOneResource($pivotResource);
+    }
+
+    public function artesCreate($id,$item)
+    {
+        $artesCreate = Arte::where('pivot_tarea_proveeder_id', $id)->first();
+        if($artesCreate)
+        {
+            return $this->successMensaje('Ya se Inicializo El Arte con este Proveedor', 201);
+        }
+        $arte = new Arte();
+        $arte->pivot_tarea_proveeder_id = $id;
+        $arte->nombre = $item;
+        $arte->creacion_fichas = 1;
+        $arte->validacion_fichas = 1;
+        $arte->creacion_boceto =  1;
+        $arte->validacion_boceto = 1;
+        $arte->confirmacion_proveedor = 1;
+        $arte->fecha_fin = Carbon::now(); /* //TODO cambiar el metodo de carbon por fecha de finalizacion recibida de request */
+        $arte->save();
+    }
+
+    public function produccionTransitoCreate($id)
+    {
+        $producionTransito = ProduccionTransito::where('pivot_tarea_proveeder_id', $id)->first();
+        if($producionTransito)
+        {
+            return $this->successMensaje('Ya se Inicializo la produccion con este Proveedor', 201);
+        }
+           $produccionAprobar = new ProduccionTransito();
+           $produccionAprobar->pivot_tarea_proveeder_id = $id;
+           $produccionAprobar->save();
         
     }
 }
