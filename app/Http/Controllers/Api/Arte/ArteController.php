584d32e2ec00920dd167c6ff512d0c63e12e19d7
kervis vasquez <kervisvasquez24@gmail.com>
2021-06-15 09:24:28 -0400

    🛂 feature/SRM-159new validations completed in update artes field fecha_fin

--- app/Http/Controllers/Api/Arte/ArteController.php
+++ app/Http/Controllers/Api/Arte/ArteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Arte;
 
+use auth;
 use App\Arte;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -24,6 +25,11 @@ class ArteController extends ApiController
 
     public function update(Request $request, Arte $arte)
     {
+        if((auth()->user()->rol != 'coordinador') && ($request->fecha_fin))
+        {
+            return $this->errorResponse('No tiene Permiso para Realizar esta Operacion', 403);
+        }
+               
         $arte->update($request->all());
         $arte->save();
         return $this->showOne($arte);
