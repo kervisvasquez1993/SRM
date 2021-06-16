6d5ec3414718c776be5c5a93da0bd3d995764949
Carlos Peña <cpea@DYNAMICS.VE>
2021-06-16 09:13:07 -0400

    :sparkles: Change task filters

--- app/Http/Resources/TareaResource.php
+++ app/Http/Resources/TareaResource.php
@@ -16,7 +16,8 @@ class TareaResource extends JsonResource
             'descripcion' => $this->descripcion,
             'fecha_fin' => $this->fecha_fin,
             'created_at' => $this->created_at,
-            'completada' => !$this->pivotTareaProveedor->where('iniciar_produccion', 1)->where('iniciar_arte', 1)->isEmpty()
+            'completada' => !$this->pivotTareaProveedor->where('iniciar_produccion', 1)->where('iniciar_arte', 1)->isEmpty(),
+            'tiene_negociacion' => !$this->pivotTareaProveedor->isEmpty()
         ];
 
         return parent::toArray($request);
