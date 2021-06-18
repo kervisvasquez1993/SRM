116aa1b9b037b46455aa5ec5805fa387735bfadb
Carlos Peña <carlos.07888@gmail.com>
2021-06-18 19:46:42 -0400

    :bug: feature/SRM-182 Fix small issues!

--- resources/js/components/Negotiation/NegotiationModal.js
+++ resources/js/components/Negotiation/NegotiationModal.js
@@ -44,7 +44,7 @@ const NegotiationModal = ({ negotiation }) => {
 
             {(iniciar_arte === 1 || iniciar_produccion === 1) && (
                 <div className="modal-footer bg-success flex-column align-items-start pl-4">
-                    {iniciar_produccion && (
+                    {iniciar_produccion === 1 && (
                         <p className="d-flex text-white align-items-center h6 pl-4">
                             <span className="material-icons mr-2">
                                 check_circle
