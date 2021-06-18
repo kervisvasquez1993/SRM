c08f376ef1ae6542e142ef33292c48077e6f40d0
Carlos Peña <carlos.07888@gmail.com>
2021-06-18 12:59:09 -0400

    :lipstick: Use SmallCard to show the resume of products and purchases on the negotiation modal

--- resources/js/components/Products/ProductsTab.js
+++ resources/js/components/Products/ProductsTab.js
@@ -1,31 +1,52 @@
 import React from "react";
+import { BiCubeAlt } from "react-icons/bi";
+import { FaWeightHanging } from "react-icons/fa";
+import { GiCardboardBox, GiWeight } from "react-icons/gi";
 import { hasNoProducts } from "../../utils";
 import EmptyList from "../Navigation/EmptyList";
+import SmallCard from "../Widgets/SmallCard";
 
 const ProductsTab = ({ negotiation }) => {
+    const { total_cbm, total_n_w, total_g_w, total_ctn } = negotiation;
+
     return (
         <React.Fragment>
             {hasNoProducts(negotiation) ? (
                 <EmptyList message="No hay productos registrados" />
             ) : (
-                <React.Fragment>
-                    <p>
-                        <strong>Total CBM : </strong>
-                        {negotiation.total_cbm}
-                    </p>
-                    <p>
-                        <strong>Total Peso Neto (kg) : </strong>
-                        {negotiation.total_n_w}
-                    </p>
-                    <p>
-                        <strong>Total Peso Bruto (kg): </strong>
-                        {negotiation.total_g_w}
-                    </p>
-                    <p>
-                        <strong>Total CTN : </strong>
-                        {negotiation.total_ctn}
-                    </p>
-                </React.Fragment>
+                <div className="row px-2">
+                    <SmallCard
+                        label="Total CBM"
+                        icon={<BiCubeAlt className="icon-normal" />}
+                        backgroundClass="bg-info"
+                    >
+                        {total_cbm}
+                    </SmallCard>
+
+                    <SmallCard
+                        label="Total Peso Neto (kg)"
+                        icon={<FaWeightHanging className="icon-normal" />}
+                        backgroundClass="bg-secondary"
+                    >
+                        {total_n_w}
+                    </SmallCard>
+
+                    <SmallCard
+                        label="Total Peso Bruto (kg)"
+                        icon={<GiWeight className="icon-normal" />}
+                        backgroundClass="bg-danger"
+                    >
+                        {total_g_w}
+                    </SmallCard>
+
+                    <SmallCard
+                        label="Total CTN"
+                        icon={<GiCardboardBox className="icon-normal" />}
+                        backgroundClass="bg-primary"
+                    >
+                        {total_ctn}
+                    </SmallCard>
+                </div>
             )}
         </React.Fragment>
     );
