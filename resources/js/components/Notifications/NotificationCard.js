2d6063ecb410657c9e3e1f99523507a25792dbff
Carlos Peña <cpea@DYNAMICS.VE>
2021-06-22 16:38:12 -0400

    :lipstick: feature/SRM-181 Add two kind of notifications

--- resources/js/components/Notifications/NotificationCard.js
+++ resources/js/components/Notifications/NotificationCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { BiTask } from "react-icons/bi";
+import { GiFactory } from "react-icons/gi";
 import { RiShip2Line } from "react-icons/ri";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
@@ -6,6 +8,14 @@ import { markAsRead } from "../../store/actions/notificationActions";
 import { getElapsedTimeString } from "../../utils";
 
 const titles = {
+    tarea_asignada: {
+        title: "Tarea Asignada",
+        icon: <BiTask className="text-primary icon-large mr-3" />
+    },
+    empresa_agregada: {
+        title: "Empresa Agregada",
+        icon: <GiFactory className="text-primary icon-large mr-3" />
+    },
     salida_puerto_origen: {
         title: "Salida de Puerto Origen",
         icon: <RiShip2Line className="text-primary icon-large mr-3" />
@@ -14,7 +24,7 @@ const titles = {
 
 const NotificationCard = ({
     id,
-    data: { body, link, tipoNotify },
+    data: { text, link, type },
     read_at,
     created_at
 }) => {
@@ -31,12 +41,12 @@ const NotificationCard = ({
         >
             <Link to={link} className="d-flex flex-row" onClick={handleClick}>
                 <div className="flex-grow-1 d-flex align-items-center">
-                    {tipoNotify && titles[tipoNotify].icon}
+                    {type && type in titles && titles[type].icon}
                     <div>
                         <h2 className="h5 font-weight-bold mb-0">
-                            {tipoNotify && titles[tipoNotify].title}
+                            {type && type in titles && titles[type].title}
                         </h2>
-                        <p>{body}</p>
+                        <p>{text}</p>
                     </div>
                 </div>
                 <div>{getElapsedTimeString(new Date(created_at))}</div>
