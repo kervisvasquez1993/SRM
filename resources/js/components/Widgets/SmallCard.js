63dc4b512df8f2e594053762c7a088003eb87d1d
Carlos Peña <cpea@DYNAMICS.VE>
2021-06-22 10:54:32 -0400

    :lipstick: feature/SRM-182 Show a message when a production is fully paid

--- resources/js/components/Widgets/SmallCard.js
+++ resources/js/components/Widgets/SmallCard.js
@@ -10,12 +10,12 @@ const SmallCard = ({
 }) => {
     return (
         <div className="d-flex align-items-center flex-column">
-            <div className={`card-icon ${backgroundClass} ${iconColor}`}>
+            <div className={`card-icon mb-1 ${backgroundClass} ${iconColor}`}>
                 {icon || <span className="material-icons">{materialIcon}</span>}
             </div>
             <div className="d-flex flex-column align-items-center text-center text-sm-left">
                 <p className="card-text font-weight-bold m-0 h5">{children}</p>
-                <p className="card-title m-0">{label}</p>
+                <p className="card-title text-center m-0">{label}</p>
             </div>
         </div>
     );
