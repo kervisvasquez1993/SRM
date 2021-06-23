f2e1bde164c21257bbfd74ca43d5f44a89ca5013
Carlos Peña <cpea@DYNAMICS.VE>
2021-06-23 16:30:24 -0400

    :lipstick: feature/SRM-181 Add more notification titles and icons

--- resources/js/components/Notifications/NotificationCard.js
+++ resources/js/components/Notifications/NotificationCard.js
@@ -1,24 +1,90 @@
 import React from "react";
 import { BiTask } from "react-icons/bi";
-import { GiFactory } from "react-icons/gi";
+import { FaRegHandshake } from "react-icons/fa";
+import { HiUpload } from "react-icons/hi";
+import { GiFactory, GiGearHammer } from "react-icons/gi";
 import { RiShip2Line } from "react-icons/ri";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { markAsRead } from "../../store/actions/notificationActions";
 import { getElapsedTimeString } from "../../utils";
+import { MdBrush } from "react-icons/md";
+import { AiFillWarning } from "react-icons/ai";
+
+const ArtIncidentIcon = () => {
+    return (
+        <div className="notification-icon-parent">
+            <AiFillWarning className="text-danger icon-large mr-3" />
+            <MdBrush className="text-primary mr-3 notification-sub-icon" />
+        </div>
+    );
+};
 
 const titles = {
     tarea_asignada: {
         title: "Tarea Asignada",
-        icon: <BiTask className="text-primary icon-large mr-3" />
+        icon: <BiTask className="text-primary icon-large mr-3 flex-shrink-0" />
     },
     empresa_agregada: {
         title: "Empresa Agregada",
-        icon: <GiFactory className="text-primary icon-large mr-3" />
+        icon: (
+            <GiFactory className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
     },
     salida_puerto_origen: {
         title: "Salida de Puerto Origen",
-        icon: <RiShip2Line className="text-primary icon-large mr-3" />
+        icon: (
+            <RiShip2Line className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
+    },
+    iniciar_negociacion: {
+        title: "Negociación Iniciada",
+        icon: (
+            <FaRegHandshake className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
+    },
+    cargar_productos: {
+        title: "Productos Cargados",
+        icon: (
+            <HiUpload className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
+    },
+    cargar_compras: {
+        title: "Órdenes de Compra Cargadas",
+        icon: (
+            <HiUpload className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
+    },
+    iniciar_arte: {
+        title: "Arte Iniciada",
+        icon: <MdBrush className="text-primary icon-large mr-3 flex-shrink-0" />
+    },
+    arte_ficha: {
+        title: "Incidencia con Creación de Fichas",
+        icon: <ArtIncidentIcon />
+    },
+    validacion_ficha: {
+        title: "Incidencia con Validación de Bocetos",
+        icon: <ArtIncidentIcon />
+    },
+    arte_boceto: {
+        title: "Incidencia con Creación de Bocetos",
+        icon: <ArtIncidentIcon />
+    },
+    validacion_boceto: {
+        title: "Incidencia con Validación de Bocetos",
+        icon: <ArtIncidentIcon />
+    },
+    confirmacion_proveedor: {
+        title: "Incidencia con Confirmación de Proveedor",
+        icon: <ArtIncidentIcon />
+    },
+
+    iniciar_produccion: {
+        title: "Producción Iniciada",
+        icon: (
+            <GiGearHammer className="text-primary icon-large mr-3 flex-shrink-0" />
+        )
     }
 };
 
@@ -46,10 +112,12 @@ const NotificationCard = ({
                         <h2 className="h5 font-weight-bold mb-0">
                             {type && type in titles && titles[type].title}
                         </h2>
-                        <p>{text}</p>
+                        <p className="m-0 mb-md-2">{text}</p>
                     </div>
                 </div>
-                <div>{getElapsedTimeString(new Date(created_at))}</div>
+                <div className="ml-2">
+                    {getElapsedTimeString(new Date(created_at))}
+                </div>
             </Link>
         </div>
     );
