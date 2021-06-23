7aff0d86c0f37a46c199aa28d5ca3233949c4b45
Carlos Peña <cpea@DYNAMICS.VE>
2021-06-23 09:01:04 -0400

    :lipstick: feature/SRM-186 Add loading screen to the notifications page

--- resources/js/store/reducers/notificationReducer.js
+++ resources/js/store/reducers/notificationReducer.js
@@ -1,5 +1,6 @@
 const defaultState = {
     list: [],
+    isLoadingList: false,
     unreadCount: 0
 };
 
@@ -7,10 +8,26 @@ const notificationReducer = (state = defaultState, action) => {
     const { type, payload } = action;
 
     switch (type) {
+        case "GET_NOTIFICATIONS_REQUEST":
+            return {
+                ...state,
+                isLoadingList: true
+            };
         case "GET_NOTIFICATIONS_SUCCESS":
             return {
                 ...state,
-                list: payload
+                list: payload,
+                isLoadingList: false
+            };
+        case "GET_NOTIFICATIONS_FAILURE":
+            return {
+                ...state,
+                isLoadingList: false
+            };
+        case "CHANGE_HISTORY":
+            return {
+                ...state,
+                isLoadingList: true
             };
 
         case "GET_UNREAD_NOTIFICATIONS_COUNT_SUCCESS":
