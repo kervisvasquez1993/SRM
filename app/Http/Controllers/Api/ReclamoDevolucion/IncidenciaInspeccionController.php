73d00e4bc0d30481114e7fdbd30ed1ae0f3bcec2
kervis vasquez <kervisvasquez24@gmail.com>
2021-06-24 14:32:10 -0400

    ✨ feature/SRM-171 add inspeccion de carga

--- app/Http/Controllers/Api/ReclamoDevolucion/IncidenciaInspeccionController.php
+++ app/Http/Controllers/Api/ReclamoDevolucion/IncidenciaInspeccionController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Api\ReclamoDevolucion;
 
+use App\User;
 use App\InspeccionCarga;
 use Illuminate\Http\Request;
 use App\RecepcionReclamoDevolucion;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ApiController;
+use App\Notifications\GeneralNotification;
 use App\Http\Requests\IncidenciaValidacion;
+use Illuminate\Support\Facades\Notification;
 
 class IncidenciaInspeccionController extends ApiController
 {
@@ -30,6 +33,16 @@ class IncidenciaInspeccionController extends ApiController
         ]);
         $test = $request->all();
         $recepcion_mercancia = InspeccionCarga::create($test);   
+        /* notificacion */
+        $login_user = auth()->user()->name;
+        $comprador_asignado = User::find($reclamos_devolucione->ProduccionTransito->pivotTable->tarea->user_id);
+        $nombre_empresa = $reclamos_devolucione->ProduccionTransito->pivotTable->proveedor->nombre;
+        $user_coordinador = User::where('rol', 'coordinador')->get();
+        $user_all = $user_coordinador->push($comprador_asignado)->unique('id');
+        $body = "El usuario '$login_user' agrego una incidencia relacionado con la inspección de mercancia con la empresa '$nombre_empresa'";
+        $link = "/claims/?id=$reclamos_devolucione->id&tab=inspeccion_carga";
+        $tipoNotify = "inspeccion_carga";
+        Notification::send($user_all, new GeneralNotification($body, $link, $tipoNotify));
         return $this->showOne($recepcion_mercancia);  
         
     }
