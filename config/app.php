e0ab5afb95efe9ce2e12ef4e4b899187875587c3
kervis vasquez <kervisvasquez24@gmail.com>
2021-06-28 17:38:10 -0400

    📦️ feature/SRM-168 lib for backup in databese

--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
 
     ],
 
