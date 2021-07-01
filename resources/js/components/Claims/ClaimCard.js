62f18a4ca6ea11fbbbc92557d463e5aa15d8d7ad
Carlos Peña <carlos.07888@gmail.com>
2021-07-01 13:13:13 -0400

    :lipstick: feature/SRM-204 Swap ClaimCard checkboxes with a progress bar

--- resources/js/components/Claims/ClaimCard.js
+++ resources/js/components/Claims/ClaimCard.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { AiOutlineBarcode } from "react-icons/ai";
+import { AiOutlineBarcode, AiOutlineZoomIn } from "react-icons/ai";
+import { FaBoxOpen, FaInfo } from "react-icons/fa";
+import { GiCheckMark } from "react-icons/gi";
+import { ImInfo, ImNewspaper } from "react-icons/im";
+import { CgClose } from "react-icons/cg";
+import { IoInformation } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { StringParam, useQueryParam } from "use-query-params";
 import { updateClaim } from "../../store/actions/claimActions";
@@ -11,6 +16,7 @@ import {
 } from "../../utils";
 import NegotiationTabs from "../Negotiation/NegotiationTabs";
 import ClaimManagementModal from "./ClaimManagementModal";
+import { Link } from "react-router-dom";
 
 const ClaimCard = ({ claim }) => {
     const dispatch = useDispatch();
@@ -50,14 +56,14 @@ const ClaimCard = ({ claim }) => {
         );
     };
 
-    const handleOpenManagement = () => {
-        dispatch(
-            openModal({
-                title: getNegotiationModalName(pivot),
-                body: <ClaimManagementModal claimId={claim.id} />
-            })
-        );
-    };
+    // const handleOpenManagement = () => {
+    //     dispatch(
+    //         openModal({
+    //             title: getNegotiationModalName(pivot),
+    //             body: <ClaimManagementModal claimId={claim.id} />
+    //         })
+    //     );
+    // };
 
     const handleCheck = e => {
         const data = {
@@ -77,89 +83,166 @@ const ClaimCard = ({ claim }) => {
             className={`card my-2 fade-in py-2 ${
                 isClaimCompleted(claim) ? "bg-success text-white" : ""
             } ${focusClassName}`}
-            onClick={handleOpenManagement}
+            onClick={handleOpenInfo}
             style={{ cursor: "pointer" }}
             ref={ref}
         >
-            <div className="card-header ml-2">
-                <div className="col-sm h4 d-flex mb-3">
+            <div className="card-header ml-2 d-flex align-items-center mb-4">
+                <div className="h4 m-0 d-flex">
                     <AiOutlineBarcode className="icon-normal mr-2" />
                     <p className="mb-0">
                         Codigo : <strong>{compra_po}</strong>
                     </p>
                 </div>
+
+                <div className="flex-grow-1 d-flex justify-content-end align-items-center">
+                    <button
+                        className="btn btn-info btn-round d-sm-none"
+                        onClick={handleOpenInfo}
+                    >
+                        <FaInfo className="icon-small" />
+                    </button>
+
+                    <button
+                        className="btn btn-sm btn-info btn-round d-none d-sm-block"
+                        onClick={handleOpenInfo}
+                    >
+                        <FaInfo className="icon-small mr-2" />
+                        Información
+                    </button>
+                </div>
             </div>
 
             <div className="card-body py-0 my-0 ml-2">
-                <div className="form-check form-check p-1">
-                    <label
-                        className="form-check-label"
-                        onClick={handleCheckClick}
-                    >
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            id="recepcion_mercancia"
-                            onChange={handleCheck}
-                            checked={recepcion_mercancia}
-                        />
-                        Recepción de Mercancía
-                        <span className="form-check-sign">
-                            <span className="check"></span>
-                        </span>
-                    </label>
-                </div>
+                {/* <div className="col-md d-md-flex justify-content-around">
+                        <div className="form-check form-check p-1">
+                            <label
+                                className="form-check-label"
+                                onClick={handleCheckClick}
+                            >
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    id="recepcion_mercancia"
+                                    onChange={handleCheck}
+                                    checked={recepcion_mercancia}
+                                />
+                                Recepción de Mercancía
+                                <span className="form-check-sign">
+                                    <span className="check"></span>
+                                </span>
+                            </label>
+                        </div>
 
-                <div className="form-check form-check p-1">
-                    <label
-                        className="form-check-label"
-                        onClick={handleCheckClick}
-                    >
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            id="inspeccion_carga"
-                            onChange={handleCheck}
-                            checked={inspeccion_carga}
-                        />
-                        Inspección de Carga
-                        <span className="form-check-sign">
-                            <span className="check"></span>
-                        </span>
-                    </label>
-                </div>
+                        <div className="form-check form-check p-1">
+                            <label
+                                className="form-check-label"
+                                onClick={handleCheckClick}
+                            >
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    id="inspeccion_carga"
+                                    onChange={handleCheck}
+                                    checked={inspeccion_carga}
+                                />
+                                Inspección de Carga
+                                <span className="form-check-sign">
+                                    <span className="check"></span>
+                                </span>
+                            </label>
+                        </div>
 
-                <div className="form-check form-check p-1">
-                    <label
-                        className="form-check-label"
-                        onClick={handleCheckClick}
-                    >
-                        <input
-                            className="form-check-input"
-                            type="checkbox"
-                            id="reclamos_devoluciones"
-                            onChange={handleCheck}
-                            checked={reclamos_devoluciones}
-                        />
-                        Reclamos y Devoluciones
-                        <span className="form-check-sign">
-                            <span className="check"></span>
-                        </span>
-                    </label>
-                </div>
-            </div>
+                        <div className="form-check form-check p-1">
+                            <label
+                                className="form-check-label"
+                                onClick={handleCheckClick}
+                            >
+                                <input
+                                    className="form-check-input"
+                                    type="checkbox"
+                                    id="reclamos_devoluciones"
+                                    onChange={handleCheck}
+                                    checked={reclamos_devoluciones}
+                                />
+                                Reclamos y Devoluciones
+                                <span className="form-check-sign">
+                                    <span className="check"></span>
+                                </span>
+                            </label>
+                        </div>
+                    </div> */}
 
-            <div className="card-footer">
-                <div className="d-flex justify-content-end align-items-center w-100 flex-wrap">
-                    <button className="btn btn-success btn-round">
-                        Administrar
-                    </button>
-                    <button
-                        className="btn btn-info btn-round"
-                        onClick={handleOpenInfo}
-                    >
-                        Ver Información
-                    </button>
+                <div className="p-2 px-3">
+                    <div className="mb-5 d-flex flex-column align-items-center">
+                        <div className="progress-container w-100">
+                            <div className="bar">
+                                <div
+                                    className="bar-progress"
+                                    style={{ width: "50%" }}
+                                ></div>
+                            </div>
+
+                            <div className="progress-status progress-warning completed">
+                                <GiCheckMark className="icon-done" />
+                                <CgClose className="icon-not-done" />
+                                <p>Recepción</p>
+                            </div>
+                            <div className="progress-status progress-danger current completed">
+                                <GiCheckMark className="icon-done" />
+                                <CgClose className="icon-not-done" />
+                                <p>Inspección</p>
+                            </div>
+                            <div className="progress-status progress-success">
+                                <GiCheckMark className="icon-done" />
+                                <CgClose className="icon-not-done" />
+                                <p>Reclamos y Devoluciones</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="d-flex flex-column flex-lg-row justify-content-center mb-2">
+                        <Link
+                            to="/"
+                            className=" btn btn-sm btn-warning mx-4 flex-basis-0 flex-grow-1"
+                        >
+                            <FaBoxOpen className="icon-normal mr-2" />
+                            Recepción
+                        </Link>
+                        <Link
+                            to="/"
+                            className=" btn btn-sm btn-danger mx-4 flex-basis-0 flex-grow-1"
+                        >
+                            <AiOutlineZoomIn className="icon-normal mr-2" />
+                            Inspección
+                        </Link>
+                        <Link
+                            to="/"
+                            className=" btn btn-sm btn-success mx-4 flex-basis-0 flex-grow-1"
+                        >
+                            <ImNewspaper className="icon-normal mr-2" />
+                            Reclamos y Devoluciones
+                        </Link>
+                    </div>
+
+                    {/* <div className="d-flex justify-content-end align-items-center">
+                        <button
+                            className="btn btn-sm btn-info btn-round"
+                            onClick={handleOpenInfo}
+                        >
+                            <ImInfo className="icon-normal mr-2" />
+                            Información
+                        </button>
+                    </div> */}
+
+                    {/* <div className="d-flex justify-content-end align-items-center">
+                        <button
+                            className="btn btn-sm btn-info"
+                            onClick={handleOpenInfo}
+                        >
+                            <IoInformation className="icon-normal" />
+                        </button>
+                    </div> */}
                 </div>
             </div>
         </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,8 +20,8 @@
 
     <!-- Styles -->
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="{{asset('assets/css/material-dashboard.css?v=2.1.2')}}" rel="stylesheet" />
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> --}}
+    {{-- <link href="{{asset('assets/css/material-dashboard.css?v=2.1.2')}}" rel="stylesheet" /> --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
