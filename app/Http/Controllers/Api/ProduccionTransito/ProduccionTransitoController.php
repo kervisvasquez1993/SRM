2eef64a68590ac00c2979052ff18b55ac4301f01
kervis vasquez <kervisvasquez24@gmail.com>
2021-07-19 09:34:40 -0400

    tests/

--- app/Http/Controllers/Api/ProduccionTransito/ProduccionTransitoController.php
+++ app/Http/Controllers/Api/ProduccionTransito/ProduccionTransitoController.php
@@ -33,7 +33,7 @@ class ProduccionTransitoController extends ApiController
             ->collapse()
             ->pluck('produccionTransito')
             ->collapse(); */
-           return  $produccion_transito_user = Auth::user()->tareas()->with('pivotTareaProveedor.produccionTransito')->has('pivotTareaProveedor.produccionTransito')->get()->pluck('pivotTareaProveedor')->collapse();
+           $produccion_transito_user = Auth::user()->tareas()->with('pivotTareaProveedor.produccionTransito')->has('pivotTareaProveedor.produccionTransito')->get()->pluck('pivotTareaProveedor')->collapse();
         }        
         $produccionTransitoResource = ProduccionTransitoResource::collection($produccion_transito_user);
         return $this->showAllResources($produccionTransitoResource); 
