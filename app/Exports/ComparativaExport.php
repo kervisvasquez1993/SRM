f4f4fc3954b46491dbcd56154eddc11785391833
carlo697 <carlos.07888@gmail.com>
2021-09-01 10:15:58 -0400

    :alembic: develop Try to fix an issue with timeout

--- app/Exports/ComparativaExport.php
+++ app/Exports/ComparativaExport.php
@@ -245,36 +245,31 @@ class ComparativaExport implements WithEvents, WithPreCalculateFormulas
                                 $sheet->getRowDimension($filaProducto)->setRowHeight(100);
 
                                 //      Agregar la imagen
-                                if ($producto->imagen) {
-                                    $url = "https://srmdnamics-laravel-file.s3.us-east-2.amazonaws.com/{$producto->imagen}";
+                                // if ($producto->imagen) {
+                                //     $url = "https://srmdnamics-laravel-file.s3.us-east-2.amazonaws.com/{$producto->imagen}";
 
-                                    $config = stream_context_create(['http' => [
-                                        'timeout' => 60,
-                                    ]]);
-            
-                                    $imagen = file_get_contents($url, false, $config);
-                                    // $imagen = file_get_contents($url);
-                                    $imagen = imagecreatefromstring($imagen);
-                                    $ancho = imagesx($imagen);
-                                    $alto = imagesy($imagen);
+                                //     $imagen = file_get_contents($url);
+                                //     $imagen = imagecreatefromstring($imagen);
+                                //     $ancho = imagesx($imagen);
+                                //     $alto = imagesy($imagen);
 
-                                    // Insertarla en el excel
-                                    $coordenada = getCoordinate($columna_inicio, $filaProducto);
-                                    $drawing = new MemoryDrawing();
-                                    $drawing->setResizeProportional(true);
-                                    $drawing->setImageResource($imagen);
-                                    $drawing->setCoordinates($coordenada);
-                                    $drawing->setOffsetX(2);
-                                    $drawing->setOffsetY(2);
+                                //     // Insertarla en el excel
+                                //     $coordenada = getCoordinate($columna_inicio, $filaProducto);
+                                //     $drawing = new MemoryDrawing();
+                                //     $drawing->setResizeProportional(true);
+                                //     $drawing->setImageResource($imagen);
+                                //     $drawing->setCoordinates($coordenada);
+                                //     $drawing->setOffsetX(2);
+                                //     $drawing->setOffsetY(2);
 
-                                    if ($ancho > $alto) {
-                                        $drawing->setWidth(120);
-                                    } else {
-                                        $drawing->setHeight(120);
-                                    }
+                                //     if ($ancho > $alto) {
+                                //         $drawing->setWidth(120);
+                                //     } else {
+                                //         $drawing->setHeight(120);
+                                //     }
 
-                                    $drawing->setWorksheet($writer->getActiveSheet());
-                                }
+                                //     $drawing->setWorksheet($writer->getActiveSheet());
+                                // }
 
                                 $productosAgregados++;
                             }
