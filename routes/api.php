6bea2da96d71d32d45f6a7f7c44ee79bb619b4e1
carlo697 <carlos.07888@gmail.com>
2021-10-04 10:09:31 -0400

    :coffin: SRM-256 Remove unused model and migration

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
  */
 
 Route::get('/proveedor', 'Api\ProveedorController@index');
-Route::get('/filter', 'Api\FilterProduccionTransitoController@index');
-
 Route::post('login', 'Api\AuthController@login');
 
 Route::middleware('auth:api')->group(function () {
