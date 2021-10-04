c6277ce53b294ed737fa19154adc75db1da5874c
carlo697 <carlos.07888@gmail.com>
2021-10-04 09:28:02 -0400

    :coffin: Remove unneeded seeders and migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,31 +16,5 @@ class DatabaseSeeder extends Seeder
     {
         
         $this->call(UserSeeder::class);
-        /*
-        $this->call(TareaSeeder::class); 
-        $this->call(ProveedorSeeder::class); 
-        $this->call(PivotSeeder::class); 
-        $this->call(EstatusSeeder::class);
-        $this->call(ArteSeeder::class);
-        $this->call(BocetoSeeder::class);
-        $this->call(PerfilSeeder::class);
-        $this->call(ValidacionBocetoSeeder::class);
-        $this->call(FichaSeeder::class);
-        $this->call(ValidacionFichaSeeder::class);
-        $this->call(InicioProduccionSeeder::class);
-        $this->call(ConfirmacionProveedorSeeder::class);
-        $this->call(ProduccionTransitoSeeder::class);
-        $this->call(PagoAnticipadoSeeder::class);
-        $this->call(PagoBalanceSeeder::class);
-        $this->call(TransitoNacionalizacionSeeder::class);
-        $this->call(FinProduccionSeeder::class);
-        $this->call(DevolucionReclamosDevolucionesSeeder::class);
-        $this->call(RecepcionReclamosDevolucionesSeeder::class);
-        $this->call(RecepcionMercanciaSeeder::class);
-        $this->call(InspeccionCargaSeeder::class);
-        $this->call(ReclamosDevolucionesSeeder::class);
-        $this->call(ProductoSeeder::class);
-        */
-        /* $this->call(ProduccionTransitoSeeder::class);  */
     }
 }
